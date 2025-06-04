32a887a130fa302690fadbd1a5f553b95fd26b2a
RhiAlves <145845129+RhiAlves@users.noreply.github.com>
2025-06-04 15:32:07 -0300

    Add files via upload

---
+++ Helicoptero.java
@@ -0,0 +1,77 @@
+public class Helicoptero{
+    private boolean estaLigado;
+    private float altitude;
+    private int capacidadeHelicoptero;
+    private int pessoasPresentes;
+
+public Helicoptero( int capacidadeHelicoptero){
+    this.capacidadeHelicoptero = capacidadeHelicoptero;
+}
+
+//getters e setters
+public boolean getEstaLigado(){
+    return estaLigado;
+}
+public void setEstaLigado(boolean estaLigado){
+    this.estaLigado = estaLigado;
+}
+public float getAltitude(){
+    return altitude;
+}
+public void setAltitude(float altitude){
+    this.altitude = altitude;
+}
+public int capacidadeHelicoptero(){
+    return capacidadeHelicoptero;
+}
+public void setCapacidadeHelicoptero(int capacidadeHelicoptero){
+    this.capacidadeHelicoptero = capacidadeHelicoptero;
+}
+public int getPessoasPresentes(){
+    return pessoasPresentes;
+}
+public void setPessoasPresentes(int pessoasPresentes){
+    this.pessoasPresentes = pessoasPresentes;
+}
+
+//metodos
+public void adicionarPessoa(){
+    if (pessoasPresentes < capacidadeHelicoptero){
+        pessoasPresentes = pessoasPresentes + 1; 
+    }else {System.out.println("Não é possível adicionar mais pessoas");}
+}
+
+public void sairPessoa(){
+    if (pessoasPresentes > 0) {
+        pessoasPresentes = pessoasPresentes - 1;
+    }
+}
+public void ligar(){
+    if (estaLigado == false){
+        estaLigado = true;
+    } else {
+            System.out.println("O helicoptero já está ligado!");
+            }
+    
+    }
+public void decolarHelicoptero(double altura) {
+    if (altitude <= 0 && estaLigado == true) {
+        altitude = (float)(altitude + altura);  
+        }
+    }
+public void aterrissarHelicoptero(double altura) {
+    if (estaLigado == false && altitude >= 1) {
+        altitude = (float)(altitude - altura); 
+    }
+    }
+public void desligarHelicoptero() {
+        if (estaLigado == true && altitude == 0) {
+            estaLigado = false;
+    }
+    }
+    @Override
+    public String toString(){
+    return "O helicoptero possui a capacipade para " + capacidadeHelicoptero + " pessoas, porem, atualmente ha " + getPessoasPresentes() + " pessoas presentes." +"\n Ele está ligado?" + estaLigado + "\n Qual a sua altitude? " + altitude +" metros.";
+}
+
+}
---
+++ PrincipalHelicoptero.java
@@ -0,0 +1,55 @@
+public class PrincipalHelicoptero {
+    public static void main(String[] args) {
+        Helicoptero h1 = new Helicoptero(5);
+        Helicoptero h2 = new Helicoptero(3);
+
+        //estado atual do Helicóptero
+        System.out.println(h1.toString());
+        System.out.println(h2.toString());
+        
+        //adicionando pessoas 
+        h1.adicionarPessoa();
+        h1.adicionarPessoa();
+        h1.adicionarPessoa();
+        h1.adicionarPessoa();
+        h1.adicionarPessoa();
+
+        h2.adicionarPessoa();
+        h2.adicionarPessoa();
+        h2.adicionarPessoa();
+        
+        //ligar helicópteros
+        h1.ligar();  
+        h2.ligar();  
+
+        //Decolar helicópteros
+        h1.decolarHelicoptero(600);  
+        h2.decolarHelicoptero(550);
+        
+        //estado dos helicópteros
+        System.out.println(h1.toString());
+        System.out.println(h2.toString());  
+        
+        //Aterrissar helicópteros
+        h1.aterrissarHelicoptero(600);
+        h2.aterrissarHelicoptero(550);
+        
+        //desligar helicópteros
+        h1.desligarHelicoptero();
+        h2.desligarHelicoptero();
+
+        //remove pessoas
+        h1.sairPessoa();
+        h1.sairPessoa();
+        h1.sairPessoa();
+        h1.sairPessoa();
+        h1.sairPessoa();
+        h2.sairPessoa();
+        h2.sairPessoa();
+        h2.sairPessoa();
+
+        //estado atual do helicóptero
+        System.out.println(h1.toString());
+        System.out.println(h2.toString());
+    }
+}
